bf77d495b6a9fe9f70e854bf4999517758526003
Hao Wu <hao.wu@bengroup.com>
2016-12-21 11:33:41 -0800

    quartile

---
+++ statsHack/quartile.py
@@ -0,0 +1,48 @@
+"""
+Sample Input
+9
+3 7 8 5 12 14 21 13 18
+
+Sample Output
+6
+12
+16
+"""
+
+
+def get_quartile(arr_int_sorted):
+    #print(arr_int_sorted)
+
+    count = len(arr_int_sorted)
+
+    # even
+    if 0 == count % 2:
+        median = (arr_int_sorted[int(count/2 - 1)] + arr_int_sorted[int(count/2)]) / 2
+        first_half = arr_int_sorted[:int(count/2)]
+        second_half = arr_int_sorted[int(count/2):]
+
+    # odd
+    else:
+        #print(int(count / 2-1))
+        median = arr_int_sorted[int(count/2)]
+        #print(median)
+        first_half = arr_int_sorted[:int(count / 2)]
+        #print(first_half)
+        second_half = arr_int_sorted[int(count / 2 + 1):]
+        #print(second_half)
+
+    return first_half, second_half, int(median)
+
+n = input()
+inp = input()
+inp = '3 7 8 5 12 14 21 13 18'
+
+inp = list(map(int, inp.split(' ')))
+inp_sorted = sorted(inp)
+
+first_half, second_half, q2 = get_quartile(inp_sorted)
+_, _, q1 = get_quartile(first_half)
+_, _, q3 = get_quartile(second_half)
+
+#print(first_half, second_half)
+print(q1, q2, q3, sep='\n')
